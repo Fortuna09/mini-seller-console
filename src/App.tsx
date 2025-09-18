cdf85a74186b3d965e948d9269af52eb819e7c56
Fortuna09 <rafael.fortuna@sou.unifal-mg.edu.br>
2025-09-18 13:43:55 -0300

    feat: integrate LeadsTable component into App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,37 @@
+import { useEffect, useState } from "react";
+import { getLeads, type Lead } from './services/api';
+import { LeadsTable } from './features/leads/LeadsTable';
+
 function App() {
+
+  const [leads, setLeads] = useState<Lead[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+  const fetchLeads = async () => {
+    try {
+      const fetchedLeads = await getLeads();
+      setLeads(fetchedLeads);
+    } catch (error) {
+      console.error("Falha ao buscar os leads:", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  fetchLeads();
+}, []);
+
+  if (isLoading) {
+    return <div className="text-center p-8">Carregando leads...</div>;
+  }
+
   return (
-    <div className="bg-gray-900 text-white min-h-screen p-8">
-      <h1 className="text-3xl font-bold">Console</h1>
+    <div className="p-8">
+      <h1 className="text-3xl font-bold text-blue-400 mb-6">Meus Leads</h1>
+      <LeadsTable leads={leads} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
